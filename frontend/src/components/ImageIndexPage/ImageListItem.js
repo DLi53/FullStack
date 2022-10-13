1250a360f999a3905a68f177a6dbc8698aca0256
DLi53 <106249295+DLi53@users.noreply.github.com>
2022-10-13 11:32:29 -0400

    render pictures

--- frontend/src/components/ImageIndexPage/ImageListItem.js
+++ frontend/src/components/ImageIndexPage/ImageListItem.js
@@ -7,12 +7,13 @@ const ImageListItem = ({image}) => {
     return ( 
         <div className="imageListItem" key={image.id}>
             <ul>
-                <li>ID: {image.id}</li>
-                <li>Title: {image.title}</li>
-                <li>Description: {image.description}</li>
-                <li>Uploader_id: {image.uploaderId}</li>
+                <li><Link to={`/pins/${image.id}`}><img className="imgIdx" src={image.photoUrl} alt="" /></Link></li>
+                {/* <li>ID: {image.id}</li> */}
+                {/* <li>Title: {image.title}</li> */}
+                {/* <li>Description: {image.description}</li>
+                <li>Uploader_id: {image.uploaderId}</li> */}
+                <br />
                 {/* <li>URL: {image.photoUrl}</li> */}
-                <Link to={`/pins/${image.id}`}><img className="imgIdx" src={image.photoUrl} alt="" /></Link>
                 <br />
             </ul>
         </div>
--- frontend/src/components/ImageIndexPage/index.js
+++ frontend/src/components/ImageIndexPage/index.js
@@ -13,13 +13,13 @@ const ImageIndexPage = () => {
     },[])
 
 // console.log(images);
-    const imageList = images.map(image => <ImageListItem image={image} key={image.id}/>)
+    const imageListItems = images.map(image => <ImageListItem image={image} key={image.id}/>)
 
     return ( 
         <div className="imageIndexPage">
             <ul className="imagesList">
-                <img className='deleteThis' src="https://imstresst-dev.s3.amazonaws.com/Screen+Shot+2022-10-12+at+9.25.24+AM.png" alt="" />
-                {imageList}
+                {/* <img className='deleteThis' src="https://imstresst-dev.s3.amazonaws.com/Screen+Shot+2022-10-12+at+9.25.24+AM.png" alt="" /> */}
+                {imageListItems}
             </ul>
 
         </div>
