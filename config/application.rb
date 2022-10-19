185f4f9c55cd479a5b05a1ac1a60f1ea7a0efcb7
DLi53 <106249295+DLi53@users.noreply.github.com>
2022-10-19 18:28:06 -0400

    update name on backend

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Winkterest
+module MyIntrest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -37,7 +37,7 @@ module Winkterest
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
-      key: '_winkterest_session',
+      key: '_myintrest_session',
       same_site: :lax, 
       secure: Rails.env.production?
 
