83ee727be129f108b682992a8407cc727bffcfe0
DLi53 <106249295+DLi53@users.noreply.github.com>
2022-12-07 15:59:59 -0500

    follows

--- frontend/src/components/SplashPage/splishCSS.js
+++ frontend/src/components/SplashPage/splishCSS.js
@@ -7,21 +7,17 @@ const SplishCss = () => {
 
     let video = document.getElementById("myVideo")
 
-    // video.play()
 
-    // const playVideo = () => {
-    //     video.play()
-    // }
 
 
 
     return ( 
         <div >
             <video autoPlay muted loop id="myVideo" >
-                {/* <source src={Video} type="video/mp4"/> */}
                 <source src="https://imstresst-dev.s3.amazonaws.com/PinterestMov/PinterestCover.mov" type="video/mp4"/>
-
             </video>
+
+            
         </div>
      );
 }
